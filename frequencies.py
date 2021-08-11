cc45c3e31204222beced5fa48dd75de3c2dd472e
Thanoscar7908 <thanoscar7908@gmail.com>
2021-08-11 09:27:19 -0700

    cleaning data

--- frequencies.py
+++ frequencies.py
@@ -1,4 +1,54 @@
 import sys
- if argv.sys == ("spam")
-        print("spam")
 
+ALPHABET = 'abcefghijklmnopqristuvwxyz'
+
+args = sys.argv[1:]
+
+for arg in args:
+
+    print(arg)
+
+text = ' '.join(args)
+
+print(text)
+
+lowercase_letter = text[0].lower()
+
+print(lowercase_letter)
+
+#lowercase_text = [letter.lower() for letter in text]
+lowercase_text = text.lower()
+
+print(lowercase_text)
+
+' '.join(lowercase_text)
+
+frequencies = {}
+
+for letter in ALPHABET:
+
+        frequencies[letter] = 0
+
+for letter in text:
+
+        if letter not in frequencies:
+
+                frequencies[letter] = 1
+
+        else:
+             
+                frequencies[letter] += 1
+
+print(frequencies)
+
+for letter in ALPHABET:
+    
+    count = frequencies[letter]
+
+    print(letter, end=' ')
+
+    for c in range(count):
+
+            print('l', end='')
+
+    print()
