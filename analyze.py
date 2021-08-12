35f6514c11ee0e7c3934350fa43498734ad409f6
Thanoscar7908 <thanoscar7908@gmail.com>
2021-08-12 14:40:06 -0700

    analyzing

--- analyze.py
+++ analyze.py
@@ -2,29 +2,49 @@ from sys import argv as arguments
 
 ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-if len(arguments) != 3:
+if len(arguments) != 2:
 
-    print("2 arguments required!")
+    print("1 arguments required!")
     exit()
 
 from_filename = arguments[1]
-to_filename = arguments[2]
+#to_filename = arguments[2]
 
 print("input: " + from_filename)
-print("output: " + to_filename)
+#print("output: " + to_filename)
 
 from_file = open(from_filename, 'r', encoding='utf8')
-to_file = open(to_filename, 'w', encoding='utf8')
+#to_file = open(to_filename, 'w', encoding='utf8')
 
 contents = from_file.read()
 
-contents = [letter if letter.upper() in ALPHABET else '' for letter in contents]
+def analyze(contents):
 
-contents = ''.join(contents)
 
-contents = contents.upper()
+        length = float(len(contents))
 
-to_file.write(contents)
+        frequencies = {}
+
+        for letter in ALPHABET:
+
+            frequencies[letter] = 0.0
+
+        for letter in contents:
+            
+            frequencies[letter] += 1.0
+            
+        ratios = {}
+
+        for letter in ALPHABET:
+            ratios[letter] = frequencies[letter] / length
+
+    return ratios
+
+ratios = analyze(contents)
+
+    print(letter, end=' ')
+    print(ratios[letter])
+#to_file.write(contents)
 
 from_file.close()
-to_file.close()
+#to_file.close()
